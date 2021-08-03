e9b36362c6f2faff7d836e7ff76dbdd4592536ac
Fernando Isaías García Aguirre <figarcia@gmail.com>
2021-08-03 13:27:21 -0500

    I refactor the code and modify styles because not is good practice use styles in html files

--- src/app/modules/invitation/biblical-quote/biblical-quote.component.ts
+++ src/app/modules/invitation/biblical-quote/biblical-quote.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-biblical-quote',
   templateUrl: './biblical-quote.component.html',
   styleUrls: ['./biblical-quote.component.scss']
 })
-export class BiblicalQuoteComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class BiblicalQuoteComponent { }
--- src/app/modules/invitation/moment/moment.component.ts
+++ src/app/modules/invitation/moment/moment.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-moment',
   templateUrl: './moment.component.html',
   styleUrls: ['./moment.component.scss']
 })
-export class MomentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class MomentComponent { }
